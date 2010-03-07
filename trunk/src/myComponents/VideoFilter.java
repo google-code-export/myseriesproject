b6be91092128daf135540f388413bc0e61815a55
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-03-07 14:09:22 +0000

    Add VideoFilter class

---
+++ trunk/src/myComponents/VideoFilter.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myComponents;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+/**
+ *
+ * @author lordovol
+ */
+public class VideoFilter implements FilenameFilter {
+
+  public static final String[] EXTENSIONS = {"avi", "mkv"};
+
+
+  public boolean accept(File dir, String name) {
+    if(new File(dir + "/" + name).isDirectory()){
+      return true;
+    }
+    for (int i = 0; i < EXTENSIONS.length; i++) {
+      String ext = EXTENSIONS[i];
+      if (name.endsWith(ext)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
