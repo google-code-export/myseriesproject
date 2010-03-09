df433cd70e266dc68c653f636cd24321f96f7e24
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-03-09 12:01:29 +0000

    Added ImagePanel class to hold the series screenshot

---
+++ src/myComponents/ImagePanel.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myComponents;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author ssoldatos
+ */
+public class ImagePanel extends JPanel {
+  private static final long serialVersionUID = 356475743574387L;
+}
