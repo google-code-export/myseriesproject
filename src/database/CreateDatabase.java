75434e26562637ac920a6debcb5e93ab63432e42
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-03-10 17:38:50 +0000

    Add screenshot field in database create statement

--- src/database/CreateDatabase.java
+++ src/database/CreateDatabase.java
@@ -140,7 +140,8 @@ public class CreateDatabase implements Runnable {
             "[link] VARCHAR," +
             "[internetUpdate] INTEGER DEFAULT 1," +
             "[tvrage_ID] INTEGER DEFAULT 0," +
-            "[localDir] VARCHAR DEFAULT '')");
+            "[localDir] VARCHAR DEFAULT ''," +
+            "[screenshot] VARCHAR DEFAULT '')");
     MySeries.logger.log(Level.INFO, "Creating table filters");
     stmt.executeUpdate("CREATE  TABLE IF NOT EXISTS [filters] " +
             "([filter_ID] INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , " +
