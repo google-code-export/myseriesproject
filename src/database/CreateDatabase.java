6f591eacf7023cb62dd89263484061cafdf205bc
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-03-31 18:36:44 +0000

    Added sonline field in series table

--- src/database/CreateDatabase.java
+++ src/database/CreateDatabase.java
@@ -141,6 +141,7 @@ public class CreateDatabase implements Runnable {
             "[internetUpdate] INTEGER DEFAULT 1," +
             "[tvrage_ID] INTEGER DEFAULT 0," +
             "[localDir] VARCHAR DEFAULT ''," +
+            "[sonline] VARCHAR DEFAULT ''," +
             "[screenshot] VARCHAR DEFAULT '')");
     MySeries.logger.log(Level.INFO, "Creating table filters");
     stmt.executeUpdate("CREATE  TABLE IF NOT EXISTS [filters] " +
