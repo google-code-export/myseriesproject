f4d01903acebe739961036676d8a38ac9b855039
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-04-05 15:34:55 +0000

    Refactored options

--- src/tools/options/Options.java
+++ src/tools/options/Options.java
@@ -28,89 +28,243 @@ import myseries.*;
 public class Options {
 
   /**
+   * The options map
+   */
+  private static Map<String, Object> options;
+  /***********************************************
+   * CONSTANTS
+   ***********************************************/
+  /**
    * The install directory
    */
   public static String _USER_DIR_;
   /**
-   * The databases path with trailing /
+   * The databases path : "databases/"
    */
   public static final String _DB_PATH_ = "databases/";
   /**
-   * MySQL date format
+   * MySQL date format : "yyyy-MM-dd"
    */
   public static final String _MYSQL_DATE_FORMAT_ = "yyyy-MM-dd";
   /**
-   * The total columns in all tables
+   * The total columns in all tables : 16
    */
   public static final int _TOTAL_COLUMNS_ = 16;
   /**
    * The regex to use for finding episodes
+   * "(([ xeEX]|(ep)|(EP))|(([ xeEX]|(ep)|(EP))\\d*([- &])))0*"
    */
   public static String _REGEX_ = "(([ xeEX]|(ep)|(EP))|(([ xeEX]|(ep)|(EP))\\d*([- &])))0*";
-
-   /**
-   * The options map
-   */
-  private static Map<String,Object> options;
-
-  
-  public static String SEASON_SEPARATOR = " - ";
-  public static String TITLE_SEPARATOR = " - ";
-  public static String EPISODE_SEPARATOR = "x";
-
   /**
-   * The screenshots path
+   * The screenshots path : "images/"
    */
   public static final String _SCREENSHOTS_PATH_ = "images/";
+  /**
+   * The torrents path : "torrents/"
+   */
   public static final String _TORRENTS_PATH_ = "torrents/";
-
-  public static String TV_RAGE = "TvRage";
-  public static String EP_GUIDES = "EpGuides";
-  public static String GREEK = "Greek";
-  public static String ENGLISH = "English";
-  public static String TV_SUBTITLES = "TvSubtitles.net";
-  public static String SONLINE = "SubtitleOnline.com";
-
-
+  /**
+   * Tv rage name : "TvRage"
+   */
+  public static final String _TV_RAGE_NAME_ = "TvRage";
+  /**
+   * EpGuides name : "EpGuides"
+   */
+  public static final String _EP_GUIDES_NAME_ = "EpGuides";
+  /**
+   * The greek subtitles language : "Greek"
+   */
+  public static final String _GREEK_ = "Greek";
+  /**
+   * The english subtitles language : "English"
+   */
+  public static final String _ENGLISH_ = "English";
+  /**
+   * The tv subtitles name : "Tvsubtitles.net"
+   */
+  public static final String _TV_SUBTITLES_NAME_ = "TvSubtitles.net";
+  /**
+   * The tv subtitles url : "http://www.tvsubtitles.net/"
+   */
+  public static final String _TV_SUBTITLES_URL_ = "http://www.tvsubtitles.net/";
+  /**
+   * The subtitle online name : "SubtitleOnline.com"
+   */
+  public static final String _SUBTITLE_ONLINE_NAME_ = "SubtitleOnline.com";
+  /**
+   * The subtitle online url : "http://www.subtitleonline.com/"
+   */
+  //public static String SONLINE_URL = "http://www.subtitleonline.com/";
+  public static final String _SUBTITLE_ONLINE_URL_ = "C:/spyros/develop/myseriesproject/soonline/";
+  /**
+   * The eztv rss link
+   */
+  public static String _EZTV_RSS_ = "http://ezrss.it/search/index.php?";
+  /**************************************************
+   * Lists
+   **************************************************/
+  /**
+   * List of web series databases
+   */
   public static Vector<String> DB_UPDATERS = new Vector<String>();
+  /**
+   * List of web subtitle sites
+   */
   public static Vector<String> SUBTITLE_SITES = new Vector<String>();
+  /**
+   * List of available languages
+   */
   public static Vector<String> SUBTITLE_LANG = new Vector<String>();
-  public static String[] QUALITIES = {"","HDTV","720p","1080i","1080p","DSRip","DVBRip","DVDR","DVDRip","DVDScr","HR.HDTV","HR.PDTV","PDTV","SatRip","SVCD","TVRip","WebRip"};
-
+  /**
+   * List of qualities
+   */
+  public static String[] QUALITIES = {"", "HDTV", "720p", "1080i", "1080p", "DSRip", "DVBRip", "DVDR", "DVDRip", "DVDScr", "HR.HDTV", "HR.PDTV", "PDTV", "SatRip", "SVCD", "TVRip", "WebRip"};
+  /*************************************************
+   *  List of Misc options
+   *************************************************/
+  /**
+   * The season seperator in renaming episodes (default : " - ")
+   */
+  public static String SEASON_SEPARATOR = " - ";
+  /**
+   * The title seperator in renaming episodes (default : " - ")
+   */
+  public static String TITLE_SEPARATOR = " - ";
+  /**
+   * The episodes seperator in renaming episodes (default : "x")
+   */
+  public static String EPISODE_SEPARATOR = "x";
+  /************************************************
+   * User Options
+   ************************************************/
+  /**
+   * next episodes limit
+   * int default: 10
+   */
   public static String NEXT_EPISODES_LIMIT = "NEXT_EPISODES_LIMIT";
+  /**
+   * Show not seen episodes in next episodes?
+   * boolean default : false
+   */
   public static String SHOW_UNSEEN = "SHOW_UNSEEN";
+  /**
+   * Show not downloaded episodes in next episodes?
+   * boolean default : false
+   */
   public static String SHOW_UNDOWNLOADED = "SHOW_UNDOWNLOADED";
+  /**
+   * Database name
+   * String default ""
+   */
   public static String DB_NAME = "DB_NAME";
+  /**
+   * Debug mode
+   * int default:0
+   */
   public static String DEBUG_MODE = "DEBUG_MODE";
+  /**
+   * If modal windows are used
+   * boolean default true
+   */
   public static String MODAL = "MODAL";
+  /**
+   * Date format
+   * String default "dd/mm/YYYY"
+   */
   public static String DATE_FORMAT = "DATE_FORMAT";
+  /**
+   * Look and feel to use
+   * Not used yet!!!
+   */
   public static String LOOK_AND_FEEL = "LOOK_AND_FEEL";
+  /**
+   * Skin color to use
+   * Color default [240,240,240]
+   */
   public static String SKIN_COLOR = "BACKGROUND_COLOR";
+  /**
+   * Use skin color or not
+   * boolean default true;
+   */
   public static String USE_SKIN = "USE_SKIN";
+  /**
+   * Use proxy to connect to internet or not
+   * boolean default : false
+   */
   public static String USE_PROXY = "USE_PROXY";
+  /**
+   * Proxy to use
+   * string default ""
+   */
   public static String PROXY_HOST = "PROXY_HOST";
+  /**
+   * Proxy port
+   * String default ""
+   */
   public static String PROXY_PORT = "PROXY_PORT";
+  /**
+   * The split pane divider's location
+   * integer default 250
+   */
   public static String DIVIDER_LOCATION = "DIVIDER_LOCATION";
+  /**
+   * the font to use
+   * String default Arial
+   */
   public static String FONT_FACE = "FONT_FACE";
+  /**
+   * The font's size
+   * integer default 12
+   */
   public static String FONT_SIZE = "FONT_SIZE";
+  /**
+   * The table columns widths
+   * ArrayList<integer> default 100
+   */
   public static String TABLE_WIDTHS = "TABLE_WIDTHS";
+  /**
+   * The frame's state
+   * integer default JFrame.NORMAL
+   */
   public static String WINDOW_STATE = "WINDOW_STATE";
+  /**
+   * The frame's width
+   * integer default 1000
+   */
   public static String WIDTH = "WIDTH";
+  /**
+   * The frame's height
+   * integer default 600
+   */
   public static String HEIGHT = "HEIGHT";
+  /**
+   * The web database used for updating episodes' data
+   * String default Options._TV_RAGE_NAME_
+   */
   public static String INTERNET_UPDATE_DB = "INTERNET_UPDATE_DB";
+  /**
+   * Check for updates on start up
+   * boolean default true
+   */
   public static String CHECK_VERSION = "CHECK_VERSION";
+  /**
+   * the primary subtitles language
+   * String default Options._GREEK_
+   */
   public static String PRIMARY_SUB = "PRIMARY_SUB";
+  /**
+   * The web page used for downloading subtitles
+   * String default Options._SUBTITLE_ONLINE_NAME_
+   */
   public static String SUBTITLE_SITE = "SUBTITLE_SITE";
 
-
-
   static {
-    DB_UPDATERS.add(EP_GUIDES);
-    DB_UPDATERS.add(TV_RAGE);
-    SUBTITLE_LANG.add(GREEK);
-    SUBTITLE_LANG.add(ENGLISH);
-    SUBTITLE_SITES.add(TV_SUBTITLES);
-    SUBTITLE_SITES.add(SONLINE);
+    DB_UPDATERS.add(_EP_GUIDES_NAME_);
+    DB_UPDATERS.add(_TV_RAGE_NAME_);
+    SUBTITLE_LANG.add(_GREEK_);
+    SUBTITLE_LANG.add(_ENGLISH_);
+    SUBTITLE_SITES.add(_TV_SUBTITLES_NAME_);
+    SUBTITLE_SITES.add(_SUBTITLE_ONLINE_NAME_);
   }
 
   /**
@@ -122,7 +276,7 @@ public class Options {
   public static void getOptions() throws FileNotFoundException, IOException {
     Options._USER_DIR_ = System.getProperties().getProperty("user.dir");
     System.out.println(_USER_DIR_);
-    options = new HashMap<String,Object>();
+    options = new HashMap<String, Object>();
     if (!new File(Options._USER_DIR_ + "/MySeries.ini").isFile()) {
       writeDefaultIniFile();
     }
@@ -159,7 +313,7 @@ public class Options {
     String[] arr = w.split(",");
     try {
       for (int i = 0; i < Options._TOTAL_COLUMNS_; i++) {
-        widths.add(i,Integer.parseInt(arr[i].trim()));
+        widths.add(i, Integer.parseInt(arr[i].trim()));
       }
       return widths;
     } catch (NumberFormatException ex) {
@@ -173,9 +327,9 @@ public class Options {
   }
 
   private static ArrayList<Integer> getDefaultColumnWidths() {
-    ArrayList<Integer>  widths = new ArrayList<Integer>();
+    ArrayList<Integer> widths = new ArrayList<Integer>();
     for (int i = 0; i < Options._TOTAL_COLUMNS_; i++) {
-      widths.add(i,100);
+      widths.add(i, 100);
     }
     return widths;
   }
@@ -318,10 +472,10 @@ public class Options {
     out.println(Options.WINDOW_STATE + " = " + JFrame.NORMAL);
     out.println(Options.WIDTH + " = 1000");
     out.println(Options.HEIGHT + " = 600");
-    out.println(Options.INTERNET_UPDATE_DB + "= TvRage");
+    out.println(Options.INTERNET_UPDATE_DB + "=" + Options._TV_RAGE_NAME_);
     out.println(Options.CHECK_VERSION + " = true");
     out.println(Options.PRIMARY_SUB + " = gr");
-    out.println(Options.SUBTITLE_SITE + " = TvSubtitles.net");
+    out.println(Options.SUBTITLE_SITE + " = " + Options._SUBTITLE_ONLINE_NAME_);
     out.close();
   }
 
