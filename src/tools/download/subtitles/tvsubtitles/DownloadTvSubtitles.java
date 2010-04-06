ad0f9ca4b821c17248ea6ca3ea7e52286b1cab58
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-04-06 14:24:37 +0000

    Fixed imports

--- src/tools/download/subtitles/tvsubtitles/DownloadTvSubtitles.java
+++ src/tools/download/subtitles/tvsubtitles/DownloadTvSubtitles.java
@@ -4,29 +4,15 @@
  */
 package tools.download.subtitles.tvsubtitles;
 
-import java.awt.Desktop;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-import java.util.zip.ZipInputStream;
 import javax.swing.JOptionPane;
-import javax.swing.JProgressBar;
 import myComponents.MyMessages;
 import myComponents.MyUsefulFunctions;
 import tools.download.subtitles.AbstractDownloadSubtitle;
