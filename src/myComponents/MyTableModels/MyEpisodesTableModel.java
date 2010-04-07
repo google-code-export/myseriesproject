192c1a1005b851bfa7ca1082fbeba8417f32066a
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-04-07 16:55:51 +0000

    Used Episode constants

--- src/myComponents/MyTableModels/MyEpisodesTableModel.java
+++ src/myComponents/MyTableModels/MyEpisodesTableModel.java
@@ -2,22 +2,24 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package myComponents;
+package myComponents.MyTableModels;
 
 import javax.swing.table.DefaultTableModel;
+import myseries.episodes.Episodes;
 
 /**
  * The table model for Episodes table
  * @author lordovol
  */
 public class MyEpisodesTableModel extends DefaultTableModel {
+
   private static final long serialVersionUID = 467462375687L;
 
   @Override
   public Class<?> getColumnClass(int columnIndex) {
-    if (columnIndex == 3 || columnIndex == 5) {
+    if (columnIndex == Episodes.DOWNLOADED_COLUMN || columnIndex == Episodes.SEEN_COLUMN) {
       return Boolean.class;
-    } else if (columnIndex == 0) {
+    } else if (columnIndex == Episodes.EPISODE_NUM_COLUMN) {
       return Integer.class;
     } else {
       return super.getColumnClass(columnIndex);
