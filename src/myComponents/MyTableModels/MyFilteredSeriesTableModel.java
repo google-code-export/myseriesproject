1ca481f852ced91ea16c01eedc2c6ee47ae4c711
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-04-07 16:46:11 +0000

    Used filteredSeries constants

--- src/myComponents/MyTableModels/MyFilteredSeriesTableModel.java
+++ src/myComponents/MyTableModels/MyFilteredSeriesTableModel.java
@@ -2,23 +2,24 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package myComponents;
+package myComponents.MyTableModels;
 
 import javax.swing.table.DefaultTableModel;
+import myseries.filters.FilteredSeries;
 
 /**
  * The tablemodel for filtered series table
  * @author lordovol
  */
 public class MyFilteredSeriesTableModel extends DefaultTableModel {
+
   private static final long serialVersionUID = 325756637473L;
 
   @Override
   public Class<?> getColumnClass(int columnIndex) {
-    if (columnIndex == 4 || columnIndex == 6) {
-
+    if (columnIndex == FilteredSeries.DOWNLOADED_COLUMN || columnIndex == FilteredSeries.SEEN_COLUMN) {
       return Boolean.class;
-    } else if (columnIndex == 1) {
+    } else if (columnIndex == FilteredSeries.EPISODE_NUMBER_COLUMN) {
       return Integer.class;
     } else {
       return super.getColumnClass(columnIndex);
@@ -27,7 +28,7 @@ public class MyFilteredSeriesTableModel extends DefaultTableModel {
 
   @Override
   public boolean isCellEditable(int row, int col) {
-    if(col == 4 || col == 5 || col ==6){
+    if (col == FilteredSeries.DOWNLOADED_COLUMN || col == FilteredSeries.SEEN_COLUMN || col == FilteredSeries.SUBS_COLUMN) {
       return true;
     }
     return false;
