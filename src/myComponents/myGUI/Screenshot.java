a1c510b2064f93f3b4bb7f1c47345af3d31aa749
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-04-09 18:41:03 +0000

    Added relocate() method

--- src/myComponents/myGUI/Screenshot.java
+++ src/myComponents/myGUI/Screenshot.java
@@ -7,9 +7,11 @@ package myComponents.myGUI;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.util.logging.Level;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import myseries.MySeries;
 
 /**
  * The screenshot panel
@@ -23,6 +25,7 @@ public class Screenshot extends JPanel {
   public static final String PATH ="images/";
 
   private static final long serialVersionUID = 356475743574387L;
+
   private Image image;
 
   /**
@@ -64,5 +67,22 @@ public class Screenshot extends JPanel {
     this.image = image;
     this.setSize(width, height);
     repaint();
+  }
+
+   public static void relocate(MySeries m) {
+     try {
+      Thread.sleep(100);
+       try {
+      int width = MySeries.splitPane_main.getDividerLocation() - 26;
+      int height = (int) (m.screenshot.getHeight(m) * ((double) width / (double) m.screenshot.getWidth(m)));
+      int yPos = (int) MySeries.table_series.getPreferredSize().getHeight() + 20;
+      //imageLayerPanel.setBounds(0, yPos, width, height);
+      m.imagePanel.setBounds(0, yPos, width, height);
+      m.imagePanel.setImage(m.screenshot, width, height);
+    } catch (NullPointerException ex) {
+    }
+    } catch (InterruptedException ex) {
+      myseries.MySeries.logger.log(Level.SEVERE, null, ex);
+    }
   }
 }
