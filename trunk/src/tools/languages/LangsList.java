d6d2c1fd9896bf1768cf14d99c1801c9923611df
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-05-24 08:16:16 +0000

    Renaming episodes gets default separators values from options (fixes issue 44)

--- trunk/src/tools/languages/LangsList.java
+++ trunk/src/tools/languages/LangsList.java
@@ -19,24 +19,24 @@ public class LangsList {
 
   public LangsList() {
     list = new ArrayList<Language>();
-    list.add(new Language("Arabic", "ar", Language.NO_PRIORITY,4));
-    list.add(new Language("Brazilian", "br", Language.NO_PRIORITY,5));
-    list.add(new Language("Bulgarian", "bg", Language.NO_PRIORITY,6));
-    list.add(new Language("Dutch", "nl", Language.NO_PRIORITY,7));
-    list.add(new Language("English", "en", Language.NO_PRIORITY,1));
-    list.add(new Language("French", "fr", Language.NO_PRIORITY,8));
-    list.add(new Language("German", "de", Language.NO_PRIORITY,9));
-    list.add(new Language("Greek", "gr", Language.NO_PRIORITY,2));
-    list.add(new Language("Hungarian", "hu", Language.NO_PRIORITY,10));
-    list.add(new Language("Italian", "it", Language.NO_PRIORITY,11));
-    list.add(new Language("Korean", "ko", Language.NO_PRIORITY,12));
-    list.add(new Language("Polish", "pl", Language.NO_PRIORITY,13));
-    list.add(new Language("Portuguese", "pt", Language.NO_PRIORITY,14));
-    list.add(new Language("Romanian", "ro", Language.NO_PRIORITY,15));
-    list.add(new Language("Russian", "ru", Language.NO_PRIORITY,16));
-    list.add(new Language("Spanish", "es", Language.NO_PRIORITY,17));
-    list.add(new Language("Turkish", "tr", Language.NO_PRIORITY,18));
-    list.add(new Language("Ukrainian", "ua", Language.NO_PRIORITY,19));
+    list.add(new Language("Arabic", "ar", Language.NO_PRIORITY, 4));
+    list.add(new Language("Brazilian", "br", Language.NO_PRIORITY, 5));
+    list.add(new Language("Bulgarian", "bg", Language.NO_PRIORITY, 6));
+    list.add(new Language("Dutch", "nl", Language.NO_PRIORITY, 7));
+    list.add(new Language("English", "en", Language.NO_PRIORITY, 1));
+    list.add(new Language("French", "fr", Language.NO_PRIORITY, 8));
+    list.add(new Language("German", "de", Language.NO_PRIORITY, 9));
+    list.add(new Language("Greek", "gr", Language.NO_PRIORITY, 2));
+    list.add(new Language("Hungarian", "hu", Language.NO_PRIORITY, 10));
+    list.add(new Language("Italian", "it", Language.NO_PRIORITY, 11));
+    list.add(new Language("Korean", "ko", Language.NO_PRIORITY, 12));
+    list.add(new Language("Polish", "pl", Language.NO_PRIORITY, 13));
+    list.add(new Language("Portuguese", "pt", Language.NO_PRIORITY, 14));
+    list.add(new Language("Romanian", "ro", Language.NO_PRIORITY, 15));
+    list.add(new Language("Russian", "ru", Language.NO_PRIORITY, 16));
+    list.add(new Language("Spanish", "es", Language.NO_PRIORITY, 17));
+    list.add(new Language("Turkish", "tr", Language.NO_PRIORITY, 18));
+    list.add(new Language("Ukrainian", "ua", Language.NO_PRIORITY, 19));
   }
 
   /**
@@ -105,9 +105,10 @@ public class LangsList {
   }
 
   public static Language getLanguageById(int id) {
-    if(id == 0 ){
+    if (id == 0) {
       return NONE;
-    } if(id == 3){
+    }
+    if (id == 3) {
       return MULTIPLE;
     }
     for (Iterator<Language> it = list.iterator(); it.hasNext();) {
@@ -121,5 +122,15 @@ public class LangsList {
 
   private Language getDefaultLanguage(int priority) {
     return list.get(priority);
+  }
+
+  public static boolean isLanguageCode(String code) {
+    for (Iterator<Language> it = list.iterator(); it.hasNext();) {
+      Language language = it.next();
+      if (language.getCode().equals(code)) {
+        return true;
+      }
+    }
+    return false;
   }
 }
