c05e69804523b9bf4b62b8e7f81f9809d6188dc9
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-06-14 09:08:29 +0000

    Added help. fixed screenshot scaling

--- trunk/src/myComponents/myGUI/MyScaledImage.java
+++ trunk/src/myComponents/myGUI/MyScaledImage.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package myComponents.myGUI;
 
 import java.awt.Image;
@@ -12,24 +11,25 @@ import java.awt.Image;
  * @author lordovol
  */
 public class MyScaledImage {
-  private  Image image;
-  private  int width;
-  private  int height;
+
+  private Image image;
+  private int width;
+  private int height;
 
   public MyScaledImage(Image image) {
     this.image = image;
   }
 
   public void fitImageIn(int fitWidth, int fitHeight) {
-    int imWidth = image.getWidth(null);
-    int imHeight = image.getHeight(null);
-    if(imWidth > imHeight){
-      this.setWidth(fitWidth);
-      this.setHeight((this.getWidth() * imHeight) / imWidth);
-    } else {
-      this.setHeight(fitHeight);
-      this.setWidth((this.getHeight() * imWidth) / imHeight);
+    float imWidth = image.getWidth(null);
+    float imHeight = image.getHeight(null);
+    while(imWidth > fitWidth || imHeight > fitHeight){
+     imWidth-=imWidth/imHeight;
+     imHeight-=1;
     }
+    this.setWidth((int)imWidth);
+    this.setHeight((int)imHeight);
+
     this.setImage(getImage().getScaledInstance(this.getWidth(), this.getHeight(), Image.SCALE_SMOOTH));
   }
 
@@ -74,6 +74,4 @@ public class MyScaledImage {
   public void setHeight(int height) {
     this.height = height;
   }
-
-
 }
