f2ed193cf210020e8ec7923b44adf20095a2da19
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-10-31 11:09:44 +0000

    Feed tab

---
+++ src/myseries/actions/FeedsActions.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myseries.actions;
+
+/**
+ *
+ * @author lordovol
+ */
+public class FeedsActions {
+
+  public static void addFeedPanel(){
+    
+  }
+
+  private FeedsActions() {
+  }
+
+}
