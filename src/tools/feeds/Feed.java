25f1b83787dbc78ad12b93f173004c4b64540fdd
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-10-31 11:11:39 +0000

    Feeds

---
+++ src/tools/feeds/Feed.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tools.feeds;
+
+/**
+ *
+ * @author lordovol
+ */
+public class Feed {
+
+  public static final String FEEDS_PATH = "feeds";
+}
