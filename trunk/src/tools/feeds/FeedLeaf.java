5990aee82a9f22b115a2f19a0192c9e7bb8b0ace
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-10-31 11:11:39 +0000

    Feeds

---
+++ trunk/src/tools/feeds/FeedLeaf.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tools.feeds;
+
+/**
+ *
+ * @author lordovol
+ */
+public class FeedLeaf {
+
+  public int id;
+  public String title;
+  public String url;
+}
