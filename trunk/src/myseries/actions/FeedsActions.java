1bc2dd2f65f6067863a981499f5e2f6331d9dcdb
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-10-31 12:16:08 +0000

    Added AdminFeed to add feeds

--- trunk/src/myseries/actions/FeedsActions.java
+++ trunk/src/myseries/actions/FeedsActions.java
@@ -5,14 +5,19 @@
 
 package myseries.actions;
 
+import database.FeedsRecord;
+
 /**
  *
  * @author lordovol
  */
 public class FeedsActions {
 
-  public static void addFeedPanel(){
-    
+  public static boolean addFeedPanel(FeedsRecord f){
+    myseries.MySeries.glassPane.activate(null);
+    AdminFeed a = new AdminFeed(f);
+    myseries.MySeries.glassPane.deactivate();
+    return a.isFeedSaved;
   }
 
   private FeedsActions() {
