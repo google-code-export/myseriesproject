689860075536a8cbeb2a461b16bf018ba490ec13
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2010-11-10 10:43:03 +0000

    Adding mouse listenet to schedule table

---
+++ trunk/src/myseries/schedule/ScheduleMouseListener.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myseries.schedule;
+
+import com.googlecode.scheduler.ScheduleDay;
+import database.DBHelper;
+import database.EpisodesRecord;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTable;
+import myComponents.MyUsefulFunctions;
+
+/**
+ *
+ * @author ssoldatos
+ */
+public class ScheduleMouseListener extends MouseAdapter {
+
+  @Override
+  public void mouseClicked(MouseEvent e) {
+    Vector<EpisodesRecord> episodes;
+    if (e.getButton() == MouseEvent.BUTTON3) {
+      JTable table = (JTable) e.getSource();
+      Point p = e.getPoint();
+      int row = table.rowAtPoint(p);
+      int col = table.columnAtPoint(p);
+      ScheduleDay val = (ScheduleDay) table.getModel().getValueAt(row, col);
+      String date = val.getYear() + "-"
+          + MyUsefulFunctions.padLeft(val.getMonth(), 2, "0") + "-"
+          + MyUsefulFunctions.padLeft(val.getDay(), 2, "0");
+
+      try {
+        episodes = DBHelper.getEpisodesBySql("SELECT * FROM episodes WHERE aired ='" + date + "'");
+        for (Iterator<EpisodesRecord> it = episodes.iterator(); it.hasNext();) {
+          EpisodesRecord ep = it.next();
+          System.out.println(ep);
+        }
+      } catch (SQLException ex) {
+        myseries.MySeries.logger.log(Level.SEVERE, null, ex);
+      }
+    }
+  }
+}
