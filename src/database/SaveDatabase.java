06075670d776d7075ef4803dd072d906ad040318
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2011-03-20 11:10:39 +0000

    Fixed database actions messages

--- src/database/SaveDatabase.java
+++ src/database/SaveDatabase.java
@@ -21,91 +21,91 @@ import tools.options.Options;
  */
 public class SaveDatabase {
 
-  
-  private String name;
-  public boolean backUp;
+    private String name;
+    public boolean backUp;
 
-  /**
-   * Brings up the panel for saving a database
-   */
-  public SaveDatabase() {
-    try {
-      showSavePane();
-    } catch (FileNotFoundException ex) {
-      MySeries.logger.log(Level.SEVERE, "Could not save database", ex);
-      MyMessages.error("Database not saved!!!", "The database could not be saved (" + ex.getMessage() + ")");
-    } catch (IOException ex) {
-      MySeries.logger.log(Level.SEVERE, "Could not save database", ex);
-      MyMessages.error("Database not saved!!!", "The database could not be saved (" + ex.getMessage() + ")");
-    }
-  }
-
-  /**
-   * Saves a database
-   * @param dbName The database name
-   */
-  public SaveDatabase(String dbName) {
-    try {
-      String source = Options._USER_DIR_ +  Database.PATH + dbName;
-      String dest = Options._USER_DIR_  + Database.PATH + dbName + Database.BACK_UP_EXT;
-      if (MyUsefulFunctions.copyfile(source, dest)) {
-        MySeries.logger.log(Level.INFO, "Database backed up!!!");
-        MyMessages.message("Database backed up", "A back up of the older database was taken");
-        backUp = true;
-        return;
-      } else {
-      }
-    } catch (FileNotFoundException ex) {
-      Logger.getLogger(SaveDatabase.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (IOException ex) {
-      Logger.getLogger(SaveDatabase.class.getName()).log(Level.SEVERE, null, ex);
-    }
-  }
-
-  private boolean overwriteDatabase() {
-    File db = new File(Options._USER_DIR_ + Database.PATH + name + Database.EXT);
-    if (db.isFile()) {
-      return MyMessages.question("File Exists",
-          "File already exists.\nOverwrite it?") == JOptionPane.NO_OPTION ? true : false;
-    } else {
-      return false;
-    }
-  }
-
-  private void commitSave() {
-    try {
-      String source = Options._USER_DIR_+ Database.PATH + Options.toString(Options.DB_NAME);
-      String dest = Options._USER_DIR_  + Database.PATH + name + Database.EXT;
-      if (MyUsefulFunctions.copyfile(source, dest)) {
-        MySeries.logger.log(Level.INFO, "Database saved");
-        MyMessages.message("Database saved", "The database was saved with the name " + name + Database.EXT);
-        return;
-      } else {
-      }
-    } catch (FileNotFoundException ex) {
-      MySeries.logger.log(Level.SEVERE, "The database could not be saved", ex);
-      MyMessages.error("Database not saved!!!", "The database could not be saved (" + ex.getMessage() + ")");
-    } catch (IOException ex) {
-      MySeries.logger.log(Level.SEVERE, "The database could not be saved", ex);
-      MyMessages.error("Database not saved!!!", "The database could not be saved (" + ex.getMessage() + ")");
-    }
-
-  }
-
-  private void showSavePane() throws FileNotFoundException, IOException {
-    name = MyUsefulFunctions.getInput("Save Database", "Save Database As ");
-    if (name == null) {
-      MySeries.logger.log(Level.INFO, "Save Database aborted");
-    } else {
-      if (!overwriteDatabase()) {
-        if ((name).equals(Options.toString(Options.DB_NAME).replace(Database.EXT, ""))) {
-          MyMessages.error("Error", "Cannot save the database on itself!!!");
-          MySeries.logger.log(Level.WARNING, "Cannot save the database on itself!!!");
-        } else {
-          commitSave();
+    /**
+     * Brings up the panel for saving a database
+     */
+    public SaveDatabase() {
+        try {
+            showSavePane();
+        } catch (FileNotFoundException ex) {
+            MySeries.logger.log(Level.SEVERE, "Could not save database", ex);
+            MyMessages.error("Database not saved!!!", "The database could not be saved (" + ex.getMessage() + ")");
+        } catch (IOException ex) {
+            MySeries.logger.log(Level.SEVERE, "Could not save database", ex);
+            MyMessages.error("Database not saved!!!", "The database could not be saved (" + ex.getMessage() + ")");
         }
-      } else {
-      }
     }
-  }
+
+    /**
+     * Saves a database
+     * @param dbName The database name
+     */
+    public SaveDatabase(String dbName) {
+        try {
+            String source = Options._USER_DIR_ + Database.PATH + dbName;
+            String dest = Options._USER_DIR_ + Database.PATH + dbName + Database.BACK_UP_EXT;
+            if (MyUsefulFunctions.copyfile(source, dest)) {
+                MySeries.logger.log(Level.INFO, "Database backed up!!!");
+                MyMessages.message("Database backed up", "A back up of the older database was taken");
+                backUp = true;
+                return;
+            } else {
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(SaveDatabase.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(SaveDatabase.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private boolean overwriteDatabase() {
+        File db = new File(Options._USER_DIR_ + Database.PATH + name + Database.EXT);
+        if (db.isFile()) {
+            return MyMessages.question("File Exists",
+                    "File already exists.\nOverwrite it?") == JOptionPane.NO_OPTION ? true : false;
+        } else {
+            return false;
+        }
+    }
+
+    private void commitSave() {
+        try {
+            String source = Options._USER_DIR_ + Database.PATH + Options.toString(Options.DB_NAME);
+            String dest = Options._USER_DIR_ + Database.PATH + name + Database.EXT;
+            if (MyUsefulFunctions.copyfile(source, dest)) {
+                MySeries.logger.log(Level.INFO, "Database saved");
+                MyMessages.message("Database saved", "The database was saved with the name " + name + Database.EXT);
+                return;
+            } else {
+            }
+        } catch (FileNotFoundException ex) {
+            MySeries.logger.log(Level.SEVERE, "The database could not be saved", ex);
+            MyMessages.error("Database not saved!!!", "The database could not be saved (" + ex.getMessage() + ")");
+        } catch (IOException ex) {
+            MySeries.logger.log(Level.SEVERE, "The database could not be saved", ex);
+            MyMessages.error("Database not saved!!!", "The database could not be saved (" + ex.getMessage() + ")");
+        }
+
+    }
+
+    private void showSavePane() throws FileNotFoundException, IOException {
+        name = MyUsefulFunctions.getInput("Save Database", "Save Database As ");
+        if (name == null) {
+            MyMessages.error("Save Database", "Save Database aborted");
+            MySeries.logger.log(Level.INFO, "Save Database aborted");
+        } else {
+            if (!overwriteDatabase()) {
+                if ((name).equals(Options.toString(Options.DB_NAME).replace(Database.EXT, ""))) {
+                    MyMessages.error("Error", "Cannot save the database on itself!!!");
+                    MySeries.logger.log(Level.WARNING, "Cannot save the database on itself!!!");
+                } else {
+                    commitSave();
+                }
+            } else {
+            }
+        }
+    }
 }
--- src/myseries/actions/DatabaseActions.java
+++ src/myseries/actions/DatabaseActions.java
@@ -22,44 +22,49 @@ import tools.options.Options;
  */
 public class DatabaseActions {
 
-  public static void createDatabase(MySeries m, boolean createNewDb) {
-    MySeries.glassPane.activate(null);
-    new StartPanel(m, createNewDb);
-  }
-
-  public static void loadDatabase(MySeries m) {
-    try {
-      String[] filter = {".db"};
-      String load = MyUsefulFunctions.getSelectedFile(database.Database.PATH, filter, "Load Database", "Select the database to load");
-      if (load!= null && !load.equals("null") && !load.equals(Options.toString(Options.DB_NAME))) {
-        if (DBConnection.checkDatabase(load)) {
-          Options.setOption(Options.DB_NAME, load);
-          Options.save();
-          m.dispose();
-          new MySeries();
-        } else {
-          MySeries.logger.log(Level.WARNING, "Selected database is invlid.Not loading...");
-          MyMessages.error("Invalid Database", "The database you selected is invalid");
-          loadDatabase(m);
-        }
-      } else {
-      }
-    } catch (ClassNotFoundException ex) {
-      MySeries.logger.log(Level.SEVERE, null, ex);
-    } catch (SQLException ex) {
-      MySeries.logger.log(Level.SEVERE, null, ex);
-    } catch (InstantiationException ex) {
-      MySeries.logger.log(Level.SEVERE, null, ex);
-    } catch (IllegalAccessException ex) {
-      MySeries.logger.log(Level.SEVERE, null, ex);
-    } catch (UnsupportedLookAndFeelException ex) {
-      MySeries.logger.log(Level.SEVERE, null, ex);
-    } catch (IOException ex) {
-      MySeries.logger.log(Level.SEVERE, null, ex);
+    public static void createDatabase(MySeries m, boolean createNewDb) {
+        MySeries.glassPane.activate(null);
+        new StartPanel(m, createNewDb);
     }
-  }
 
-  public static void saveDatase() {
-    new SaveDatabase();
-  }
+    public static void loadDatabase(MySeries m) {
+        try {
+            MySeries.glassPane.activate(null);
+            String[] filter = {".db"};
+            String load = MyUsefulFunctions.getSelectedFile(database.Database.PATH, filter, "Load Database", "Select the database to load");
+            if (load != null && !load.equals("null") && !load.equals(Options.toString(Options.DB_NAME))) {
+                if (DBConnection.checkDatabase(load)) {
+                    Options.setOption(Options.DB_NAME, load);
+                    Options.save();
+                    m.dispose();
+                    new MySeries();
+                } else {
+                    MySeries.logger.log(Level.WARNING, "Selected database is invlid.Not loading...");
+                    MyMessages.error("Invalid Database", "The database you selected is invalid");
+                    loadDatabase(m);
+                }
+            } else {
+            }
+        } catch (ClassNotFoundException ex) {
+            MySeries.logger.log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            MySeries.logger.log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            MySeries.logger.log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            MySeries.logger.log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            MySeries.logger.log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            MySeries.logger.log(Level.SEVERE, null, ex);
+        } finally {
+            MySeries.glassPane.deactivate();
+        }
+    }
+
+    public static void saveDatase() {
+        MySeries.glassPane.activate(null);
+        new SaveDatabase();
+        MySeries.glassPane.deactivate();
+    }
 }
