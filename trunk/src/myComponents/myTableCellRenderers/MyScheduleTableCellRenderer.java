072c6d99fb5faf69bf7c586265a9ac5b2f682312
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2011-03-22 18:50:54 +0000

    Changed schedule colors

--- trunk/src/myComponents/myTableCellRenderers/MyScheduleTableCellRenderer.java
+++ trunk/src/myComponents/myTableCellRenderers/MyScheduleTableCellRenderer.java
@@ -8,7 +8,9 @@ import com.googlecode.scheduler.ScheduleDay;
 import com.googlecode.scheduler.SchedulerCellRenderer;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Font;
 import java.util.Calendar;
+import java.util.Date;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JTable;
@@ -21,53 +23,66 @@ import tools.Skin;
  */
 public class MyScheduleTableCellRenderer extends SchedulerCellRenderer {
 
-  public ScheduleDayPanel panel;
-  private int cellHeight;
-  private int cellWidth;
-  private Color disabledBack = Color.LIGHT_GRAY;
-  private Color selBack = Skin.getSkinColor();
-  private Color selFore = Skin.getColor_5();
-  private Color plainBack = Skin.getColor_1();
-  private Color plainFore = Skin.getColor_5();
-  private Color nowBack = Skin.getColor_5();
-  private Color nowFore = Skin.getColor_1();
+    public ScheduleDayPanel panel;
+    private int cellHeight;
+    private int cellWidth;
+    private Color disabledBack = Color.LIGHT_GRAY;
+    private Color selBack = Skin.getColor_5();
+    private Color selFore = Skin.getColor_1();
+    private Color plainBack = Skin.getColor_1();
+    private Color plainFore = Skin.getColor_5();
+    private Color nowBack = Skin.getSkinColor();
+    private Color nowFore = Skin.getColor_5();
+    private Color weekendBack = Skin.getColor_4();
+    private Color weekendFore = Skin.getColor_2();
 
-  @Override
-  public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-    JLabel dayLabel = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-    dayLabel.setOpaque(false);
-    dayLabel.setBorder(BorderFactory.createEmptyBorder());
-    this.cellHeight = table.getRowHeight();
-    this.cellWidth = table.getColumnModel().getColumn(column).getWidth();
-    panel = new ScheduleDayPanel(value, dayLabel, cellHeight, cellWidth);
-    panel.setBorder(BorderFactory.createEmptyBorder());
-    //panel.setBackground(plainBack);
-    //dayLabel.setForeground(plainFore);
-    if (value instanceof ScheduleDay) {
-      ScheduleDay s = (ScheduleDay) value;
-      if (isToday(s)) {
-        panel.setBackground(nowBack);
-        dayLabel.setForeground(nowFore);
-      } else {
-        if (isSelected) {
-          panel.setBackground(selBack);
-          dayLabel.setForeground(selFore);
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        JLabel dayLabel = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        dayLabel.setOpaque(false);
+        dayLabel.setBorder(BorderFactory.createEmptyBorder());
+        this.cellHeight = table.getRowHeight();
+        this.cellWidth = table.getColumnModel().getColumn(column).getWidth();
+        panel = new ScheduleDayPanel(value, dayLabel, cellHeight, cellWidth);
+        panel.setBorder(BorderFactory.createEmptyBorder());
+
+        //panel.setBackground(plainBack);
+        //dayLabel.setForeground(plainFore);
+        if (value instanceof ScheduleDay) {
+            ScheduleDay s = (ScheduleDay) value;
+            Date date = s.getDate();
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(date);
+            int day = cal.get(Calendar.DAY_OF_WEEK);
+
+            if (isSelected) {
+                panel.setBackground(selBack);
+                dayLabel.setForeground(selFore);
+            } else {
+                if (isToday(s)) {
+                    //dayLabel.setForeground(nowFore);
+                    panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                    panel.setBackground(nowBack);
+                } else {
+                    if (day == Calendar.SATURDAY || day == Calendar.SUNDAY) {
+                        dayLabel.setForeground(Skin.getColor_5());
+                        dayLabel.setFont(dayLabel.getFont().deriveFont(Font.BOLD,dayLabel.getFont().getSize()+2));
+                    } else {
+                        panel.setBackground(plainBack);
+                        dayLabel.setForeground(plainFore);
+                    }
+                }
+            }
         } else {
-          panel.setBackground(plainBack);
-          dayLabel.setForeground(plainFore);
+            panel.setBackground(disabledBack);
         }
-      }
 
-    } else {
-      panel.setBackground(disabledBack);
+        return panel;
     }
 
-    return panel;
-  }
-
-  private boolean isToday(ScheduleDay s) {
-    return Calendar.getInstance().get(Calendar.DATE) == s.getDay()
-            && Calendar.getInstance().get(Calendar.MONTH) + 1 == s.getMonth()
-            && Calendar.getInstance().get(Calendar.YEAR) == s.getYear();
-  }
+    private boolean isToday(ScheduleDay s) {
+        return Calendar.getInstance().get(Calendar.DATE) == s.getDay()
+                && Calendar.getInstance().get(Calendar.MONTH) + 1 == s.getMonth()
+                && Calendar.getInstance().get(Calendar.YEAR) == s.getYear();
+    }
 }
