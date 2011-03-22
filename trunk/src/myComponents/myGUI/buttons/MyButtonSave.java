cc24dd6c37431ff89b5c7aef9a7071b08f25e06f
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2011-03-22 18:23:38 +0000

    Added aply and save buttons

---
+++ trunk/src/myComponents/myGUI/buttons/MyButtonSave.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myComponents.myGUI.buttons;
+
+/**
+ *
+ * @author lordovol
+ */
+public class MyButtonSave extends MyAbstractButton{
+    private static final long serialVersionUID = 346457645764674L;
+
+    public MyButtonSave() {
+        super(SAVE);
+    }
+}
