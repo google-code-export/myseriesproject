9b3a74a04e39dd5aac4d3c279c3ad0dcf0132afe
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2011-03-28 04:36:34 +0000

    Added new buttons

---
+++ trunk/src/myComponents/myGUI/buttons/MyButtonInternet.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myComponents.myGUI.buttons;
+
+/**
+ *
+ * @author lordovol
+ */
+public class MyButtonInternet extends MyAbstractButton{
+    private static final long serialVersionUID = 346457645764674L;
+
+    public MyButtonInternet() {
+        super(INTERNET);
+        setToolTipText("Update from the internet");
+    }
+}
