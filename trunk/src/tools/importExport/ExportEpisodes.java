44371492979bbbdc0f1e942965a78b7f4c00ef53
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2011-04-10 10:30:29 +0000

    Import/export episodes logging messages

--- trunk/src/tools/importExport/ExportEpisodes.java
+++ trunk/src/tools/importExport/ExportEpisodes.java
@@ -12,69 +12,68 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import myseries.*;
 import javax.swing.JFileChooser;
 import myComponents.MyMessages;
 import myComponents.myFileFilters.EpisodesExportFilter;
 import myseries.episodes.Episodes;
 import myseries.series.Series;
-import myComponents.MyUsefulFunctions;
 import tools.MySeriesLogger;
+
 /**
  * Export episodes to a file
  * @author lordovol
  */
 public class ExportEpisodes {
 
-  public static final String EXT = ".eef";
-  private File file;
+    public static final String EXT = ".eef";
+    private File file;
 
-  /**
-   * Exports the episodes of the current Series<br />
-   * It exports the episode number, the episode title and the date when the episodes was aired
-   */
-  public ExportEpisodes() {
-    MySeriesLogger.logger.log(Level.INFO, "Exporting episodes of " + Series.getCurrentSerial().getFullTitle());
-    JFileChooser f = new JFileChooser();
-    f.setApproveButtonText("Export");
-    f.setDialogTitle("Export episodes of " + Series.getCurrentSerial().getFullTitle());
-    f.setFileFilter(new EpisodesExportFilter());
-    f.setSelectedFile(new File(Series.getCurrentSerial().getFullTitle() + EXT));
-    int returnVal = f.showDialog(null, "Export");
-    f.setApproveButtonText("Export");
-    if (returnVal == JFileChooser.CANCEL_OPTION) {
-      MySeriesLogger.logger.log(Level.INFO, "Exporting aborted");
-    } else {
-      file = f.getSelectedFile();
-      MySeriesLogger.logger.log(Level.INFO, "Exporting episodes to " + file.getName());
-      try {
-        exportEpisodesToFile(file);
-      } catch (IOException ex) {
-        MySeriesLogger.logger.log(Level.SEVERE, "Could not write to file", ex);
-      } catch (SQLException ex) {
-        Logger.getLogger(ExportEpisodes.class.getName()).log(Level.SEVERE, "Could not fetch the episodes from the database", ex);
-      }
+    /**
+     * Exports the episodes of the current Series<br />
+     * It exports the episode number, the episode title and the date when the episodes was aired
+     */
+    public ExportEpisodes() {
+        MySeriesLogger.logger.log(Level.INFO, "Exporting episodes of {0}", Series.getCurrentSerial().getFullTitle());
+        JFileChooser f = new JFileChooser();
+        f.setApproveButtonText("Export");
+        f.setDialogTitle("Export episodes of " + Series.getCurrentSerial().getFullTitle());
+        f.setFileFilter(new EpisodesExportFilter());
+        f.setSelectedFile(new File(Series.getCurrentSerial().getFullTitle() + EXT));
+        int returnVal = f.showDialog(null, "Export");
+        f.setApproveButtonText("Export");
+        if (returnVal == JFileChooser.CANCEL_OPTION) {
+            MySeriesLogger.logger.log(Level.INFO, "Exporting aborted");
+        } else {
+            file = f.getSelectedFile();
+            MySeriesLogger.logger.log(Level.INFO, "Exporting episodes to {0}", file.getName());
+            try {
+                exportEpisodesToFile(file);
+            } catch (IOException ex) {
+                MySeriesLogger.logger.log(Level.SEVERE, "Could not write to file", ex);
+            } catch (SQLException ex) {
+                MySeriesLogger.logger.log(Level.SEVERE, "Could not fetch the episodes from the database", ex);
+            }
+        }
     }
-  }
 
-  /**
-   * Writes the file with the episodes
-   * @param file The file to write
-   * @throws java.io.IOException
-   * @throws java.sql.SQLException
-   */
-  private void exportEpisodesToFile(File file) throws IOException, SQLException {
-    PrintWriter out = myComponents.MyUsefulFunctions.createOutputStream(file, false);
-    ArrayList<EpisodesRecord> episodes = Episodes.getCurrentSeriesEpisodes();
-    for (int i = 0; i < episodes.size(); i++) {
-      EpisodesRecord ep = episodes.get(i);
-      out.println(
-          ep.getEpisode() + "\t"
-          + ep.getTitle() + "\t"
-          + ep.getAired());
+    /**
+     * Writes the file with the episodes
+     * @param file The file to write
+     * @throws java.io.IOException
+     * @throws java.sql.SQLException
+     */
+    private void exportEpisodesToFile(File file) throws IOException, SQLException {
+        PrintWriter out = myComponents.MyUsefulFunctions.createOutputStream(file, false);
+        ArrayList<EpisodesRecord> episodes = Episodes.getCurrentSeriesEpisodes();
+        for (int i = 0; i < episodes.size(); i++) {
+            EpisodesRecord ep = episodes.get(i);
+            out.println(
+                    ep.getEpisode() + "\t"
+                    + ep.getTitle() + "\t"
+                    + ep.getAired());
+        }
+        out.close();
+        MySeriesLogger.logger.log(Level.FINE, "Export completed");
+        MyMessages.message("Export completed", "Export of " + Series.getCurrentSerial().getFullTitle() + " is completed");
     }
-    out.close();
-    MyMessages.message("Export completed", "Export of " + Series.getCurrentSerial().getFullTitle() + " is completed");
-  }
-
 }
--- trunk/src/tools/importExport/insertEpisodesInDB.java
+++ trunk/src/tools/importExport/insertEpisodesInDB.java
@@ -14,62 +14,64 @@ import myComponents.myEvents.MyEventsClass;
 import myseries.episodes.Episodes;
 import myseries.*;
 import myComponents.MyUsefulFunctions;
+
 /**
  * The task used to import the episodes in the DB
  */
 class insertEpisodesInDB implements Runnable {
 
-  private ImportEpisodes im;
-  private MyEventsClass evClass = new MyEventsClass();
+    private ImportEpisodes im;
+    private MyEventsClass evClass = new MyEventsClass();
 
-  /**
-   * Import episodes
-   * @param im The import episodes form
-   */
-  insertEpisodesInDB(ImportEpisodes im) {
-    this.im = im;
-
-  }
-
-  public void run() {
-    try {
-      insert();
-    } catch (SQLException ex) {
-      MySeriesLogger.logger.log(Level.SEVERE, null, ex);
-    } catch (ParseException ex) {
-      MySeriesLogger.logger.log(Level.SEVERE, null, ex);
+    /**
+     * Import episodes
+     * @param im The import episodes form
+     */
+    insertEpisodesInDB(ImportEpisodes im) {
+        this.im = im;
     }
-  }
 
-  private void insert() throws SQLException, ParseException {
-    int total = im.newEpisodes.size();
-    Iterator<EpisodesRecord> it = im.newEpisodes.iterator();
-    EpisodesRecord e;
-    int i = 0;
-    int val = 0;
-    while (it.hasNext()) {
-      i++;
-      e = it.next();
-      save(e);
-      val = (i * 100) / total;
-      im.progress_import.setValue(val);
-      im.progress_import.setString(val + "%");
+    public void run() {
+        try {
+            insert();
+        } catch (SQLException ex) {
+            MySeriesLogger.logger.log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            MySeriesLogger.logger.log(Level.SEVERE, null, ex);
+        }
     }
-    im.progress_import.setString("Completed");
-    evClass.fireMyEvent(new MyEvent(this, MyEventHandler.SERIES_UPDATE));
-    Episodes.getCurrentSeriesEpisodes();
-    im.dispose();
-    MySeries.glassPane.deactivate();
-  }
 
-  private void save(EpisodesRecord e) throws SQLException {
-    int episode = e.getEpisode();
-    int series_ID = e.getSeries_ID();
-    Vector<EpisodesRecord> episodes = DBHelper.getEpisodesBySql(
-            "SELECT * from episodes WHERE episode = " + episode + " AND series_ID = " + series_ID);
-    if (episodes.size() > 0) {
-      e.setEpisode_ID(episodes.get(0).getEpisode_ID());
+    private void insert() throws SQLException, ParseException {
+        int total = im.newEpisodes.size();
+        Iterator<EpisodesRecord> it = im.newEpisodes.iterator();
+        EpisodesRecord e;
+        int i = 0;
+        int val = 0;
+        while (it.hasNext()) {
+            i++;
+            e = it.next();
+            save(e);
+            MySeriesLogger.logger.log(Level.FINE, "Saved episode {0}", e.getTitle());
+            val = (i * 100) / total;
+            im.progress_import.setValue(val);
+            im.progress_import.setString(val + "%");
+        }
+        im.progress_import.setString("Completed");
+        evClass.fireMyEvent(new MyEvent(this, MyEventHandler.SERIES_UPDATE));
+        Episodes.getCurrentSeriesEpisodes();
+        im.dispose();
+        MySeries.glassPane.deactivate();
     }
-    e.save();
-  }
+
+    private void save(EpisodesRecord e) throws SQLException {
+        int episode = e.getEpisode();
+        int series_ID = e.getSeries_ID();
+        Vector<EpisodesRecord> episodes = DBHelper.getEpisodesBySql(
+                "SELECT * from episodes WHERE episode = " + episode + " AND series_ID = " + series_ID);
+        if (episodes.size() > 0) {
+            e.setEpisode_ID(episodes.get(0).getEpisode_ID());
+        }
+        MySeriesLogger.logger.log(Level.INFO, "Saving episode {0}",e.getTitle());
+        e.save();
+    }
 }
