c4a0499c20c13f3afd81860ea1edc802ddf3549c
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2011-04-12 18:19:35 +0000

    When downloading subtitles from tvsubtitles show good/bad percentage

--- src/tools/download/subtitles/Subtitle.java
+++ src/tools/download/subtitles/Subtitle.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package tools.download.subtitles;
 
 import java.net.URL;
@@ -14,26 +13,29 @@ import myComponents.MyUsefulFunctions;
  */
 public class Subtitle {
 
-  public String title;
-  public URL url;
+    public String title;
+    public URL url;
+    public int love = 0;
+    public int hate = 0;
 
-  /**
-   *
-   * @param title
-   * @param url
-   */
-  public Subtitle(String title, URL url) {
-    this.title = MyUsefulFunctions.deleteDoubleSpaces(title);
-    this.url = url;
-  }
+    /**
+     *
+     * @param title
+     * @param url
+     */
+    public Subtitle(String title, URL url, int love, int hate) {
+        this.title = MyUsefulFunctions.deleteDoubleSpaces(title);
+        this.url = url;
+        this.love = love;
+        this.hate = hate;
+    }
 
-
-
-  @Override
-  public String toString() {
-    return title;
-  }
-
-
-
+    @Override
+    public String toString() {
+        if(love+hate == 0){
+            return title;
+        }
+        int p = love *100 /(love+hate);
+        return title + " " + p +"%";
+    }
 }
