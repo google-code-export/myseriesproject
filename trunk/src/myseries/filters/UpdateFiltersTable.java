c2354a9f4efe5488893a0572c81d1331dac1611b
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2011-04-26 09:09:53 +0000

    Changes for the non static main form components and database connection

--- trunk/src/myseries/filters/UpdateFiltersTable.java
+++ trunk/src/myseries/filters/UpdateFiltersTable.java
@@ -4,6 +4,7 @@
  */
 package myseries.filters;
 
+import database.DBConnection;
 import database.EpisodesRecord;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -11,7 +12,9 @@ import javax.swing.event.TableModelEvent;
 import javax.swing.table.TableModel;
 import tools.languages.Language;
 import myComponents.MyUsefulFunctions;
+import myseries.MySeries;
 import tools.MySeriesLogger;
+
 /**
  *
  * @author lordovol
@@ -20,7 +23,7 @@ public class UpdateFiltersTable {
 
   private TableModel model;
 
-  public UpdateFiltersTable(TableModelEvent e) {
+  public UpdateFiltersTable(TableModelEvent e, MySeries m) {
     if (e.getType() == TableModelEvent.UPDATE) {
       int row = e.getFirstRow();
       model = (TableModel) e.getSource();
@@ -33,11 +36,11 @@ public class UpdateFiltersTable {
           ep.setDownloaded(downloaded ? EpisodesRecord.DOWNLOADED : EpisodesRecord.NOT_DOWNLOADED);
           ep.setSeen(seen ? EpisodesRecord.SEEN : EpisodesRecord.NOT_SEEN);
           ep.setSubs(subs);
-          ep.save();
-         // NextEpisodes.createNextEpisodes();
-         // NextEpisodes.show();
+          ep.save(new DBConnection().stmt);
+          // NextEpisodes.createNextEpisodes();
+          // NextEpisodes.show();
           Thread.sleep(100);
-          Filters.getFilteredSeries();
+          Filters.getFilteredSeries(m.comboBox_seen, m.comboBox_filterSubtitles, m.combobox_downloaded);
         } catch (SQLException ex) {
           MySeriesLogger.logger.log(Level.WARNING, null, ex);
         } catch (InterruptedException ex) {
