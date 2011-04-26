5599c31b852f7ea1037261b454cdbb1a35150ca9
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2011-04-26 09:15:36 +0000

    Added myseries online constants

--- trunk/src/myseries/MySeriesConstants.java
+++ trunk/src/myseries/MySeriesConstants.java
@@ -17,4 +17,9 @@ public interface MySeriesConstants {
   int TAB_SCHEDULE_ID = 3;
   int TAB_FEEDS_ID = 4;
 
+  String DONATION_URL = "http://www.paypal.com";
+  String GOOGLE_CODE_URL = "http://code.google.com/p/myseriesproject/";
+  String GOOGLE_CODE_TRACKER = "http://code.google.com/p/myseriesproject/issues/list";
+  String EMAIL = "lordovol@hotmail.com";
+
 }
