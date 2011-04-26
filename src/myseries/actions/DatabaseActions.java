2344eed97d4ae8e3f6f6b213330a0fe072dd5bf2
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2011-04-26 09:05:37 +0000

    Added database path constant

--- src/myseries/actions/DatabaseActions.java
+++ src/myseries/actions/DatabaseActions.java
@@ -6,16 +6,14 @@ package myseries.actions;
 
 import database.DBConnection;
 import database.SaveDatabase;
-import java.io.IOException;
-import java.sql.SQLException;
 import java.util.logging.Level;
-import javax.swing.UnsupportedLookAndFeelException;
 import myComponents.MyMessages;
 import myComponents.MyUsefulFunctions;
 import myseries.MySeries;
 import myseries.StartPanel;
 import tools.options.Options;
 import tools.MySeriesLogger;
+import tools.options.Paths;
 
 /**
  *
@@ -23,51 +21,43 @@ import tools.MySeriesLogger;
  */
 public class DatabaseActions {
 
-    public static void createDatabase(MySeries m, boolean createNewDb) {
-        MySeries.glassPane.activate(null);
-        MySeriesLogger.logger.log(Level.INFO, "Showing createDatabase panel");
-        new StartPanel(m, createNewDb);
-    }
+  public static void createDatabase(MySeries m, boolean createNewDb) {
+    MySeries.glassPane.activate(null);
+    MySeriesLogger.logger.log(Level.INFO, "Showing createDatabase panel");
+    new StartPanel(m, createNewDb);
+  }
 
-    public static void loadDatabase(MySeries m) {
-        try {
-            MySeries.glassPane.activate(null);
-            String[] filter = {".db"};
-            MySeriesLogger.logger.log(Level.INFO, "Loading database");
-            String load = MyUsefulFunctions.getSelectedFile(database.Database.PATH, filter, "Load Database", "Select the database to load");
-            if (load != null && !load.equals("null") && !load.equals(Options.toString(Options.DB_NAME))) {
-              MySeriesLogger.logger.log(Level.INFO, "Database to load : {0}",load);
-                if (DBConnection.checkDatabase(load)) {
-                    Options.setOption(Options.DB_NAME, load);
-                    Options.save();
-                    m.dispose();
-                    new MySeries();
-                } else {
-                    MySeriesLogger.logger.log(Level.WARNING, "Selected database is invlid.Not loading...");
-                    MyMessages.error("Invalid Database", "The database you selected is invalid");
-                    loadDatabase(m);
-                }
-            } else {
-            }
-        } catch (ClassNotFoundException ex) {
-            MySeriesLogger.logger.log(Level.SEVERE, "Database class not found", ex);
-        } catch (SQLException ex) {
-            MySeriesLogger.logger.log(Level.SEVERE, "Sql exception occured", ex);
-        } catch (InstantiationException ex) {
-            MySeriesLogger.logger.log(Level.SEVERE, "Could not instantiate object", ex);
-        } catch (IllegalAccessException ex) {
-            MySeriesLogger.logger.log(Level.SEVERE, "Illegal access exception", ex);
-        } catch (UnsupportedLookAndFeelException ex) {
-            MySeriesLogger.logger.log(Level.SEVERE, "Look and feel not supported", ex);
-        } catch (IOException ex) {
-            MySeriesLogger.logger.log(Level.SEVERE, "I/O exception occured", ex);
-        } finally {
-            MySeries.glassPane.deactivate();
+  public static void loadDatabase(MySeries m) {
+    DBConnection conn = null;
+    try {
+      MySeries.glassPane.activate(null);
+      String[] filter = {".db"};
+      MySeriesLogger.logger.log(Level.INFO, "Loading database");
+      String load = MyUsefulFunctions.getSelectedFile(Paths.DATABASES_PATH, filter, "Load Database", "Select the database to load");
+      if (load != null && !load.equals("null") && !load.equals(Options.toString(Options.DB_NAME))) {
+        MySeriesLogger.logger.log(Level.INFO, "Database to load : {0}", load);
+        conn = new DBConnection(load);
+        if (conn.checkDatabase()) {
+          Options.setOption(Options.DB_NAME, load);
+          Options.save();
+          ApplicationActions.restartApplication(m);
+        } else {
+          MySeriesLogger.logger.log(Level.WARNING, "Selected database is invlid.Not loading...");
+          MyMessages.error("Invalid Database", "The database you selected is invalid");
+          loadDatabase(m);
         }
+      } else {
+      }
+    } finally {
+      MySeries.glassPane.deactivate();
+      if (conn != null) {
+        conn.close();
+      }
     }
+  }
 
-    public static void saveDatase() {
-      MySeriesLogger.logger.log(Level.INFO, "Saving database action");
-        new SaveDatabase();
-    }
+  public static void saveDatase() {
+    MySeriesLogger.logger.log(Level.INFO, "Saving database action");
+    new SaveDatabase();
+  }
 }
