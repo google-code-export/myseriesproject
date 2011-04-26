d91cdb56de7ac2b9bafd7f99a7e6377740b5b44d
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2011-04-26 08:54:03 +0000

    added new buttons

--- src/myComponents/myGUI/buttons/MyAbstractButton.java
+++ src/myComponents/myGUI/buttons/MyAbstractButton.java
@@ -29,6 +29,9 @@ public abstract class MyAbstractButton extends Button {
     public static final String INTERNET = "internet";
     public static final String RSS_ADD = "rss_add";
     public static final String RSS_REFRESH = "rss_refresh";
+    public static final String GOOGLE_CODE = "googlecode";
+    public static final String EMAIL = "mail";
+    public static final String CLEAR = "clear";
 
     private class Adapter extends MouseAdapter{
         
