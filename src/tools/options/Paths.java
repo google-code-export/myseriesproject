1042353e25368eba050bfbde491d501def496414
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2011-04-26 09:31:05 +0000

    Added paths constants

---
+++ src/tools/options/Paths.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tools.options;
+
+/**
+ *
+ * @author ssoldatos
+ */
+public interface Paths {
+
+  /**
+   * The torrents path : "torrents/"
+   */
+  public static final String TORRENTS_PATH = "torrents/";
+  /**
+   * The databases path
+   */
+  public static final String DATABASES_PATH = "databases/";
+  /**
+   * The screenshots path : "images/"
+   */
+  public static final String SCREENSHOTS_PATH = "images/";
+  /**
+   * The path to feeds : "feeds/"
+   */
+  public static final String FEEDS_PATH = "feeds/";
+  /**
+   * The path to logs : "logs/"
+   */
+  public static final String LOGS_PATH = "logs/";
+}
