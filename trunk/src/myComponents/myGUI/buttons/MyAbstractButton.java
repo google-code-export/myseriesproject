dc036afbb96b61ec2f9c9d939d0c68f9905ca296
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2011-05-04 17:46:20 +0000

    Fixed buttons

--- trunk/src/myComponents/myGUI/buttons/MyAbstractButton.java
+++ trunk/src/myComponents/myGUI/buttons/MyAbstractButton.java
@@ -4,21 +4,20 @@
  */
 package myComponents.myGUI.buttons;
 
-import buttons.Button;
 import java.awt.Cursor;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.logging.Level;
-import javax.swing.JComponent;
-import javax.swing.plaf.ComponentUI;
-import javax.swing.plaf.basic.BasicButtonUI;
-import tools.MySeriesLogger;
+import java.awt.Dimension;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.UIManager;
+import javax.swing.plaf.BorderUIResource;
+import javax.swing.plaf.ButtonUI;
 
 /**
  *
  * @author lordovol
  */
-public abstract class MyAbstractButton extends Button {
+public abstract class MyAbstractButton extends JButton {
 
     public static final String images = "/myComponents/myGUI/buttons/images/";
     public static final String OK = "ok";
@@ -36,48 +35,19 @@ public abstract class MyAbstractButton extends Button {
     public static final String EMAIL = "mail";
     public static final String CLEAR = "clear";
     public static final String SEARCH = "search";
+    Dimension size = new Dimension(32, 32);
 
-    private class Adapter extends MouseAdapter {
-
-        private Adapter() {
-        }
-
-        @Override
-        public void mouseEntered(MouseEvent e) {
-            MyAbstractButton.this.setBorderPainted(false);
-            //MyAbstractButton.this.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
-        }
-
-        @Override
-        public void mouseExited(MouseEvent e) {
-            MyAbstractButton.this.setBorderPainted(false);
-            //MyAbstractButton.this.setBorder(BorderFactory.createEmptyBorder());
-        }
-
-        @Override
-        public void mousePressed(MouseEvent e) {
-            Button b = (buttons.Button) e.getSource();
-            MySeriesLogger.logger.log(Level.INFO, "Button {0} was pressed", b.getType());
-            //  MyAbstractButton.this.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
-        }
-    }
-
-    public MyAbstractButton() {
+    public MyAbstractButton(){
         this(CANCEL);
     }
 
     public MyAbstractButton(String type) {
-        super(images, type);
-        setOpaque(false);
-        setFocusPainted(false);
+        this.setIcon(new ImageIcon(getClass().getResource(images + type +".png")));
         setCursor(new Cursor(Cursor.HAND_CURSOR));
-       // addMouseListener(new Adapter());
-    }
-
-    static class MyButtonUI extends BasicButtonUI {
-
-        public static ComponentUI createUI(JComponent c) {
-            return new MyButtonUI();
-        }
+        setMinimumSize(size);
+        setMaximumSize(size);
+        setPreferredSize(size);
+        ButtonUI b = (ButtonUI) UIManager.getDefaults().getUI(this);
+        setUI(b);
     }
 }
