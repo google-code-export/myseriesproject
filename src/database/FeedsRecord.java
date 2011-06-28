0845ee72a4d60b135a3b0ab0a3bbc3b70a090610
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2011-06-28 20:06:40 +0000

    Added query method to Recoord

--- src/database/FeedsRecord.java
+++ src/database/FeedsRecord.java
@@ -16,13 +16,13 @@ import myComponents.MyUsefulFunctions;
 import tools.options.Options;
 import tools.MySeriesLogger;
 import tools.options.Paths;
+
 /**
  *
  * @author lordovol
  */
 public class FeedsRecord extends Record {
 
-  
   /**
    * The default values for the record attributes
    */
@@ -30,11 +30,9 @@ public class FeedsRecord extends Record {
   private String title = "";
   private String url = "";
   public static final String TABLE = "feeds";
-
   public static final String FEED_ID = "feed_ID";
   public static final String TITLE = "title";
   public static final String URL = "url";
-  
 
   public FeedsRecord() {
     super();
@@ -42,48 +40,75 @@ public class FeedsRecord extends Record {
 
   public FeedsRecord(int feed_ID) {
     super();
-    if(feed_ID > 0){
-      ResultSet rs =null;
+    Statement stmt = null;
+    if (feed_ID > 0) {
+      ResultSet rs = null;
       try {
-        MySeriesLogger.logger.log(Level.INFO, "Getting feed: {0}",feed_ID);
-        String sql = "SELECT * FROM feeds WHERE feed_ID = " + feed_ID;
-        Statement stmt =  DBConnection.conn.createStatement();
-         rs = query(stmt, sql);
+        MySeriesLogger.logger.log(Level.INFO, "Getting feed: {0}", feed_ID);
+        String sql = "SELECT * FROM " + TABLE + " WHERE " + FEED_ID + " = " + feed_ID;
+        stmt = DBConnection.conn.createStatement();
+        rs = query(stmt, sql);
         while (rs.next()) {
           this.feed_ID = feed_ID;
           this.url = rs.getString("url");
           this.title = rs.getString("title");
-          MySeriesLogger.logger.log(Level.FINE, "Feed found : {0}",title);
+          MySeriesLogger.logger.log(Level.FINE, "Feed found : {0}", title);
         }
       } catch (SQLException ex) {
         MySeriesLogger.logger.log(Level.SEVERE, null, ex);
       } finally {
-        if(rs != null){
+        if (rs != null) {
           try {
             rs.close();
+
           } catch (SQLException ex) {
-            Logger.getLogger(FeedsRecord.class.getName()).log(Level.SEVERE, null, ex);
+           MySeriesLogger.logger.log(Level.SEVERE, null, ex);
+          }
+          if (stmt != null) {
+            try {
+              stmt.close();
+            } catch (SQLException ex) {
+             MySeriesLogger.logger.log(Level.SEVERE, null, ex);
+            }
           }
         }
       }
 
     }
   }
+  
+  /**
+   * Saves a feed Record
+   * @return The id of the record or -1 if it's an update
+   * @throws SQLException
+   */
+  public int save() throws SQLException, DatabaseException {
+    String sql;
+    Statement stmt = DBConnection.conn.createStatement();
+    MySeriesLogger.logger.log(Level.INFO, "Saving feed");
+    if (this.feed_ID != 0) {
+      return save(TABLE, new String[]{TITLE, URL},
+              new String[]{MyUsefulFunctions.escapeString(this.getTitle()), this.getUrl()},
+              FEED_ID + " = ?", new String[]{String.valueOf(this.feed_ID)});
+    } else {
+      return save(TABLE, new String[]{TITLE, URL}, new String[]{MyUsefulFunctions.escapeString(this.getTitle()), this.getUrl()}, null, null);
+    }
 
+  }
 
 
   public boolean delete() {
-    if(this.feed_ID > 0){
+    if (this.feed_ID > 0) {
       ResultSet rs = null;
-      MySeriesLogger.logger.log(Level.INFO, "Deleting feed : {0}",feed_ID);
+      MySeriesLogger.logger.log(Level.INFO, "Deleting feed : {0}", feed_ID);
       try {
-        File file = new File(Options._USER_DIR_+Paths.FEEDS_PATH+feed_ID);
-        if(file.exists()){
-          MySeriesLogger.logger.log(Level.INFO, "Deleting feed file : {0}" , file.getName());
+        File file = new File(Options._USER_DIR_ + Paths.FEEDS_PATH + feed_ID);
+        if (file.exists()) {
+          MySeriesLogger.logger.log(Level.INFO, "Deleting feed file : {0}", file.getName());
           file.delete();
           MySeriesLogger.logger.log(Level.FINE, "Feed file deleted");
         }
-        delete(TABLE, FEED_ID+ " = ?", new String[]{String.valueOf(this.feed_ID)});
+        delete(TABLE, FEED_ID + " = ?", new String[]{String.valueOf(this.feed_ID)});
         return true;
       } catch (SQLException ex) {
         MySeriesLogger.logger.log(Level.SEVERE, "Sql exception occured", ex);
@@ -100,14 +125,14 @@ public class FeedsRecord extends Record {
       MySeriesLogger.logger.log(Level.INFO, "Getting all feeds");
       String sql = "SELECT * FROM feeds ORDER BY title";
       Statement stmt = DBConnection.conn.createStatement();
-       rs = query(stmt, sql);
-      while(rs.next()){
+      rs = query(stmt, sql);
+      while (rs.next()) {
         FeedsRecord f = new FeedsRecord();
         f.feed_ID = rs.getInt("feed_ID");
         f.title = rs.getString("title");
         f.url = rs.getString("url");
         feeds.add(f);
-        MySeriesLogger.logger.log(Level.FINE, "Feed found : {0}",f.title);
+        MySeriesLogger.logger.log(Level.FINE, "Feed found : {0}", f.title);
       }
       return feeds;
     } catch (SQLException ex) {
@@ -122,28 +147,7 @@ public class FeedsRecord extends Record {
     }
   }
 
-
-
-
-  /**
-   * Saves a feed Record
-   * @return The id of the record or -1 if it's an update
-   * @throws SQLException
-   */
-  public int save() throws SQLException, DatabaseException {
-    String sql;
-    Statement stmt = DBConnection.conn.createStatement();
-    MySeriesLogger.logger.log(Level.INFO, "Saving feed");
-    if (this.feed_ID != 0) {
-      return save(TABLE, new String[] {TITLE, URL},
-              new String[] { MyUsefulFunctions.escapeString(this.getTitle()),this.getUrl() },
-              FEED_ID + " = ?", new String[] {String.valueOf(this.feed_ID)});
-    } else {
-         return save(TABLE, new String[] {TITLE, URL}, new String[] { MyUsefulFunctions.escapeString(this.getTitle()),this.getUrl() }, null, null);
-    }
-    
-  }
-
+  
   /**
    * @return the feed_ID
    */
