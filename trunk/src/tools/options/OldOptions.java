2f6e4db1af6ecf043bf139787517dad05904f099
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2011-10-16 18:42:24 +0000

    Fix arraytype default options

--- trunk/src/tools/options/OldOptions.java
+++ trunk/src/tools/options/OldOptions.java
@@ -441,13 +441,15 @@ public class OldOptions implements IOldOptions{
     }
   }
 
-  public static Integer[] getDefaultToolbarButtons() {
-    return new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+  public static ArrayList<Integer> getDefaultToolbarButtons() {
+    Integer[] integ =  new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
           12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+    return new ArrayList<Integer>(Arrays.asList(integ));
   }
 
-  private static Integer[] getDefaultTabsOrder() {
-    return new Integer[]{0, 1, 2, 3, 4};
+  private static  ArrayList<Integer> getDefaultTabsOrder() {
+    Integer[] integ = new Integer[]{0, 1, 2, 3, 4};
+    return new ArrayList<Integer>(Arrays.asList(integ));
   }
 
   private static void loadDefaultOptions() {
