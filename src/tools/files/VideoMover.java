8721fc8acda4485ef6b09bfe912975b62d2dcdd3
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2012-04-07 17:13:53 +0000

    Move episode videos along with subtitles

---
+++ src/tools/files/VideoMover.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tools.files;
+
+import database.SeriesRecord;
+import java.util.ArrayList;
+import myComponents.myFileFilters.SubtitlesFilter;
+import myComponents.myFileFilters.VideoFilter;
+
+/**
+ *
+ * @author ssoldatos
+ */
+public class VideoMover extends AbstractMover{
+
+  
+  public VideoMover(ArrayList<SeriesRecord> series) {
+    super(series);
+    filter = new VideoFilter();
+  }
+}
