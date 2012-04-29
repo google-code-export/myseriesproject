25ecc4101571d49126d7ed6ae02950493658518f
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2012-04-29 09:32:33 +0000

    Fix video filter

--- src/tools/files/FileMover.java
+++ src/tools/files/FileMover.java
@@ -7,6 +7,7 @@ package tools.files;
 import database.SeriesRecord;
 import java.io.File;
 import java.util.logging.Level;
+import myComponents.MyMessages;
 import myComponents.MyUsefulFunctions;
 import myseriesproject.MySeries;
 import tools.MySeriesLogger;
@@ -18,34 +19,38 @@ import tools.options.MySeriesOptions;
  */
 public class FileMover implements Runnable {
 
-    protected File file;
-    protected SeriesRecord ser;
+  protected File file;
+  protected SeriesRecord ser;
 
   public FileMover(File file, SeriesRecord ser) {
     this.file = file;
     this.ser = ser;
-    
+
   }
 
-    public void run() {
-      move();
-    }
+  public void run() {
+    move();
+  }
 
-    private boolean move() {
-      MySeriesLogger.logger.log(Level.INFO, "Move file {0} to local folder", this.file.getName());
-      long start = System.currentTimeMillis();
-      long time= 0;
-      if (this.file.renameTo(new File(this.ser.getLocalDir() + "/" + this.file.getName()))) {
-        time =  System.currentTimeMillis() - start;
-        MySeriesLogger.logger.log(Level.INFO, "Moved file {0} to local folder in {1} ms", new Object[]{this.file.getName(), time});
-        System.out.println("File:" + this.file.getName() + " in " + time);
-        if (MyUsefulFunctions.isSubtitle(this.file) &&  MySeries.options.getBooleanOption(MySeriesOptions.AUTO_RENAME_SUBS)) {
-          MyUsefulFunctions.renameEpisode(this.ser, this.file.getName());
-          return true;
-        }
-      } else {
-        MySeriesLogger.logger.log(Level.WARNING, "Could not move file {0}", this.file.getName());
-      }
+  private boolean move() {
+    MySeriesLogger.logger.log(Level.INFO, "Move file {0} to local folder", this.file.getName());
+    long start = System.currentTimeMillis();
+    long time = 0;
+    if (!this.file.canWrite()) {
+      MyMessages.warning("Cannot move file", "Cannot move file " + this.file.getName() + ". It is probably used by another application (torrent client?)", true);
+      MySeriesLogger.logger.log(Level.WARNING, "Cannot move file {0}. It is probably used by another application (torrent client?)", this.file.getName());
       return false;
     }
+    if (this.file.renameTo(new File(this.ser.getLocalDir() + "/" + this.file.getName()))) {
+      time = System.currentTimeMillis() - start;
+      MySeriesLogger.logger.log(Level.INFO, "Moved file {0} to local folder in {1} ms", new Object[]{this.file.getName(), time});
+      if (MyUsefulFunctions.isSubtitle(this.file) && MySeries.options.getBooleanOption(MySeriesOptions.AUTO_RENAME_SUBS)) {
+        MyUsefulFunctions.renameEpisode(this.ser, this.file.getName());
+        return true;
+      }
+    } else {
+      MySeriesLogger.logger.log(Level.WARNING, "Could not move file {0}", this.file.getName());
+    }
+    return false;
   }
+}
