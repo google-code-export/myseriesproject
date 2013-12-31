1180eda27e1d888061738a25112b542b731bd342
spyros@valor.gr <spyros@valor.gr@7ac3ebf0-26fb-11df-b172-d5673a86adf5>
2013-12-31 11:49:09 +0000

    Version 1.6.4 r932

--- src/myseriesproject/MySeries.java
+++ src/myseriesproject/MySeries.java
@@ -126,8 +126,8 @@ public class MySeries extends javax.swing.JFrame implements TableModelListener, 
   public MyEpisodesTableModel tableModel_episodes;
   private MyFilteredSeriesTableModel tableModel_filterSeries;
   public ComboBoxModel comboBoxModel_filters;
-  public static String version = "1.6.4(r928)";
-  public String date = "2013-12-27";
+  public static String version = "1.6.4(r932)";
+  public String date = "2013-12-31";
   public static MyDisabledGlassPane glassPane = new MyDisabledGlassPane();
   public static final long serialVersionUID = 234563636363L;
   public MyImagePanel imagePanel = new MyImagePanel(true);
